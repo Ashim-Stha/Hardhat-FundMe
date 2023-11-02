ed7fd213788f1ee71b967111733a7f956ad1db23
Ashim <ashimshrestha2384@gmail.com>
2023-11-02 10:23:19 +0545

    hardhat-deploy

---
+++ deploy/01-deploy-fund-me.js
@@ -0,0 +1,18 @@
+// function deployFunc() {
+//   console.log("HI");
+// }
+
+const { network } = require("hardhat");
+
+// module.exports.default = deployFunc;
+
+// module.exports = async (hre) => { //hre=hardhat runtime environment
+//   const { getNamedAccounts, deployments } = hre;
+//   //hre.getNamedAccounts
+// };
+
+module.exports = async ({ getNamedAccounts, deployments }) => {
+  const { deploy, log } = deployments;
+  const { deployer } = await getNamedAccounts();
+  const chainId = network.config.chainId;
+};
