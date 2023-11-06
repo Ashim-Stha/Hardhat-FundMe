7adaaf4405249c168974ba9f697dd218e5be2359
Ashim <ashimshrestha2384@gmail.com>
2023-11-06 11:37:37 +0545

    deployedMocks

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,18 +1,28 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("hardhat-deploy");
 
+const acc = "bb57886497e3d955e333f73295a1473f7a6653a4d190c216b4d479bd43350385";
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: {
     compilers: [{ version: "0.8.8" }, { version: "0.6.6" }],
   },
   defaultNetwork: "hardhat",
-  // networks: {
-  //   sepolia: {
-  //     url: "https://eth-sepolia.g.alchemy.com/v2/B8hivM5MpYmrXNZ0YKbHUJ-st8ywx2Tn",
-  //     accounts:
-  //       "bb57886497e3d955e333f73295a1473f7a6653a4d190c216b4d479bd43350385",
-  //     chainId: 11155111,
-  //   },
-  // },
+  networks: {
+    hardhat: {
+      chainId: 31337,
+      // gasPrice: 130000000000,
+    },
+    sepolia: {
+      url: "https://eth-sepolia.g.alchemy.com/v2/B8hivM5MpYmrXNZ0YKbHUJ-st8ywx2Tn",
+      accounts: [acc],
+      chainId: 11155111,
+    },
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0,
+      1: 0,
+    },
+  },
 };
--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -1,4 +1,7 @@
 const networkConfig = {
+  31337: {
+    name: "localhost",
+  },
   11155111: {
     name: "Sepolia",
     ethUsdPriceFeed: "0x694AA1769357215DE4FAC081bf1f309aDC325306",
@@ -9,6 +12,13 @@ const networkConfig = {
   },
 };
 
+const developmentChains = ["hardhat", "localhost"];
+const DECIMALS = "8";
+const INITIAL_ANSWER = "200000000000";
+
 module.exports = {
   networkConfig,
+  developmentChains,
+  DECIMALS,
+  INITIAL_ANSWER,
 };
